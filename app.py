610185ed0e2bc0cfe5e2edf32d255fee0c9b40c8
KrishS-02 <singhalkrish21@gmail.com>
2026-01-18 13:22:18 +0530

    Files updated

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+# app.py
+
 from flask import Flask, render_template, request
 from services.calculator import PremiumCalculator
 from services.risk import (
-    calculate_BMI,
+    calculate_bmi,
     bmi_multiplier,
     SMOKING_RISK,
-    ALCOHOL_RISK,
-    TOBACCO_RISK
+    TOBACCO_RISK,
+    ALCOHOL_RISK
 )
 
 app = Flask(__name__)
@@ -16,31 +18,35 @@ def home():
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
+
     age = int(request.form['age'])
     gender = request.form['gender']
 
-    height = ((float(request.form['height_feet']) * 12 + 
-               float(request.form['height_inches'])) * 0.0254)
-    weight = float(request.form['weight_kilogram']) + \
-             float(request.form['weight_grams']) / 1000
+    height_m = ((float(request.form['height_feet']) * 12 +
+                 float(request.form['height_inches'])) * 0.0254)
+
+    weight_kg = float(request.form['weight_kilogram']) + \
+                float(request.form['weight_grams']) / 1000
+
     coverage = int(request.form['coverage'])
-    bmi = calculate_BMI(weight, height)
+
+    bmi = calculate_bmi(weight_kg, height_m)
 
     total_risk_multiplier = (
-        bmi_multiplier(bmi) * 
+        bmi_multiplier(bmi) *
         SMOKING_RISK[request.form['smoker']] *
-        ALCOHOL_RISK[request.form['alcoholic']] *
-        TOBACCO_RISK[request.form['chewer']]
+        TOBACCO_RISK[request.form['chewer']] *
+        ALCOHOL_RISK[request.form['alcoholic']]
     )
+
     calculator = PremiumCalculator()
     result = calculator.calculate_premium(
         age, gender, coverage, total_risk_multiplier
     )
 
-    result['bmi'] = round(bmi,2)
-    return render_template('result.html', result= result)
+    result["bmi"] = round(bmi, 2)
 
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    return render_template("result.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- services/calculator.py
+++ services/calculator.py
@@ -1,24 +1,25 @@
+# services/calculator.py
+
 from data.mortality import MORTALITY_TABLE, get_age_group
 
 class PremiumCalculator:
 
-    def __init__(self, loading_factor = 1.25):
+    def __init__(self, loading_factor=1.25):
         self.loading_factor = loading_factor
-    
+
     def calculate_premium(self, age, gender, coverage, risk_multiplier):
         age_group = get_age_group(age)
         base_rate = MORTALITY_TABLE[gender][age_group]
 
         base_premium = coverage * base_rate
-        
         risk_adjusted = base_premium * risk_multiplier
         final_premium = risk_adjusted * self.loading_factor
-        
+
         return {
-            'age_group': age_group,
-            'base_rate': base_rate,
-            'base_premium': round(base_premium, 2),
-            'final_premium': round(final_premium, 2),
-            'monthly_premium': round(final_premium/12,2),
-            'loading': self.loading_factor
+            "age_group": age_group,
+            "base_rate": base_rate,
+            "base_premium": round(base_premium, 2),
+            "final_premium": round(final_premium, 2),
+            "monthly_premium": round(final_premium / 12, 2),
+            "loading": self.loading_factor
         }
